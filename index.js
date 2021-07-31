e0f769b647730ed3a3112e69dc1fafddb46f9902
leonmanu <leonmanu@gmail.com>
2021-07-30 21:19:47 -0300

    solo para una prueba html

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   //.get("/",  (req, res) => res.render("pages/index", { resultados }))
-  .get("/", (req, res) => res.redirect("/curso/cursoAsignaturas12ZUdrHKPal1xlOvwdVAwa-hiiTYszX9I"))
+  .get('/', (req, res) => res.render('pages/index'))
   .use("/curso", routerCurso)
   .listen(PORT, () => console.log(`Listening on ${PORT}}`));
