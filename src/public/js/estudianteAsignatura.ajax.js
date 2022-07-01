85714bc3e70370ce4006d97b14a1dd6a5d8e285e
manueuse <leonmanu@gmail.com>
2022-07-01 09:50:49 -0300

    Se limita a sólo cuentas abc

--- src/public/js/estudianteAsignatura.ajax.js
+++ src/public/js/estudianteAsignatura.ajax.js
@@ -4,126 +4,7 @@ $( window ).on( "load", function() {
 	//AJAX	obtener estudiantes///////////////////////
 	var tbl = '';
 
-
-
-	//--->make div editable > start
-	$(document).on('click', '.row_data', function(event) 
-	{
-		event.preventDefault(); 
-
-		if($(this).attr('edit_type') == 'button')
-		{
-			return false; 
-		}
-
-		//make div editable
-		$(this).closest('div').attr('contenteditable', 'true');
-		//add bg css
-		$(this).addClass('bg-warning').css('padding','5px');
-
-		$(this).focus();
-	})	
-	//--->make div editable > end
-
-
-	//--->save single field data > start
-	$(document).on('focusin', '.row_data', function(event) 
-	{
-		event.preventDefault();
-
-		if($(this).attr('edit_type') == 'button')
-		{
-			return false; 
-		}
-
-		var row_id = $(this).closest('tr').attr('row_id'); 
-		
-		var row_div = $(this)				
-		.removeClass('bg-warning') //add bg css
-		.css('padding','')
-
-		var col_name = row_div.attr('col_name'); 
-		var col_val = row_div.html(); 
-
-		var arr = {};
-		arr[col_name] = col_val;
-
-		//use the "arr"	object for your ajax call
-		$.extend(arr, {row_id:row_id});
-
-		//out put to show
-		$('.post_msg').html( '<pre class="bg-success">'+JSON.stringify(arr, null, 2) +'</pre>');
-		
-	})	
-	//--->save single field data > end
-
  
-	//--->button > edit > start	
-	$(document).on('click', '.btn_edit', function(event) 
-	{
-		event.preventDefault();
-		var tbl_row = $(this).closest('tr');
-
-		var row_id = tbl_row.attr('row_id');
-
-		tbl_row.find('.btn_save').show();
-		tbl_row.find('.btn_cancel').show();
-		tbl_row.find('.w3-select').prop("disabled", false);
-		tbl_row.find('.w3-input').prop("disabled", false);
-		//hide edit button
-		tbl_row.find('.btn_edit').hide(); 
-
-		//make the whole row editable
-		tbl_row.find('.row_data')
-		.attr('contenteditable', 'true')
-		.attr('edit_type', 'button')
-		.addClass('bg-warning')
-		.css('padding','3px')
-
-		//--->add the original entry > start
-		tbl_row.find('.row_data').each(function(index, val) 
-		{  
-			//this will help in case user decided to click on cancel button
-			$(this).attr('original_entry', $(this).html());
-		}); 		
-		//--->add the original entry > end
-
-	});
-	//--->button > edit > end
-
-
-	//--->button > cancel > start	
-	$(document).on('click', '.btn_cancel', function(event) 
-	{
-		event.preventDefault();
-
-		var tbl_row = $(this).closest('tr');
-
-		var row_id = tbl_row.attr('row_id');
-
-		//hide save and cacel buttons
-		tbl_row.find('.btn_save').hide();
-		tbl_row.find('.btn_cancel').hide();
-		tbl_row.find('.w3-select').prop("disabled", true);
-		tbl_row.find('.w3-input').prop("disabled", true);
-
-		//show edit button
-		tbl_row.find('.btn_edit').show();
-
-		//make the whole row editable
-		tbl_row.find('.row_data')
-		.attr('edit_type', 'click')
-		.removeClass('bg-warning')
-		.css('padding','') 
-
-		tbl_row.find('.row_data').each(function(index, val) 
-		{   
-			$(this).html( $(this).attr('original_entry') ); 
-		});  
-	});
-	//--->button > cancel > end
-
-	
 	//--->save whole row entery > start	
 	$(document).on('click', '.btn_save', function(event) 
 	{
@@ -132,23 +13,9 @@ $( window ).on( "load", function() {
 
 		var row_id = tbl_row.attr('row_id');
 		var rowNumber = tbl_row.attr('rowNumber');
-		//acá deberían ir los otro atributos
-		
-		//hide save and cacel buttons
-		//tbl_row.find('.btn_save').hide();
-		//tbl_row.find('.btn_cancel').hide();
+
 		tbl_row.find('.w3-select').prop("disabled", true);
 		tbl_row.find('.w3-input').prop("disabled", true);
-
-		//show edit button
-		tbl_row.find('.btn_edit').show();
-
-
-		//make the whole row editable
-		// tbl_row.find('.row_data')
-		// .attr('edit_type', 'click')
-		// .removeClass('bg-warning')
-		// .css('padding','') 
 
 		//--->get row data > start
 		var arr = {}; 
